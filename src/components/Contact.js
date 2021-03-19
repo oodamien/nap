e2b67d162ad1d5c56616aef9a986a0bdfc3a82e0
Damien Vitrac <damien@vitrac.me>
2021-03-19 15:50:18 +0100

    Up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,33 +22,33 @@ const Contact = (props) => (
                     </ul>
                 </form>
             </section> */}
-            <section className="list">
-                <section>
+            <section className="list grid-wrapper">
+                <section className="col-4">
                     <div className="contact-item">
                         <h3>Le Napoli</h3>
                         <div>
                           <span>05 58 43 77 31</span>
                           <div className="icons">
-                            <a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a>
-                            <a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a>
+                            <a href="https://www.facebook.com/Le-Napoli-201299473336419/" className="icon alt fa-facebook"><span className="label">Facebook</span></a>
+                            <a href="https://www.instagram.com/napolihossegor/" className="icon alt fa-instagram"><span className="label">Instagram</span></a>
                           </div>
                           <em>Réservation non possible</em>
                         </div>
                     </div>
                 </section>
-                    <section>
-                        <div className="contact-item">
-                            <h3>Là-Haut</h3>
-                            <div>
-                              <span>05 58 43 77 31</span><div className="icons">
-                                <a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a>
-                                <a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a>
-                              </div>
-                              <em>Réservation recommandée</em>
+                  <section className="col-4">
+                      <div className="contact-item">
+                          <h3>Là-Haut</h3>
+                          <div>
+                            <span>05 58 43 77 31</span><div className="icons">
+                              <a href="https://www.facebook.com/Le-Napoli-201299473336419/" className="icon alt fa-facebook"><span className="label">Facebook</span></a>
+                              <a href="https://www.instagram.com/lenapoli.lahaut/" className="icon alt fa-instagram"><span className="label">Instagram</span></a>
                             </div>
-                        </div>
-                    </section>
-                <section>
+                            <em>Réservation recommandée</em>
+                          </div>
+                      </div>
+                  </section>
+                <section className="col-4">
                     <div className="contact-item">
                         <h3>Adresse</h3>
                         <span>755 Avenue de la Grande Dune<br />
--- src/pages/nouveautes.js
+++ src/pages/nouveautes.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
 import pic11 from '../assets/images/pic11.jpg'
+import pic08 from '../assets/images/pic08.jpg'
+import pic09 from '../assets/images/pic09.jpg'
+import pic10 from '../assets/images/pic10.jpg'
 
 const Nouveautes = (props) => (
     <Layout alt={false}>
@@ -17,11 +21,55 @@ const Nouveautes = (props) => (
                     <header className="major">
                         <h1>Nouveautés 2021</h1>
                     </header>
-                    <span className="image main"><img src={pic11} alt="" /></span>
                     <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
                 </div>
+            </section>
+            <section id="two" className="spotlights">
+                <section>
+                    <Link to="/generic" className="image">
+                        <img src={pic08} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Orci maecenas</h3>
+                            </header>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. 
+                              Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/generic" className="image">
+                        <img src={pic09} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Rhoncus magna</h3>
+                            </header>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. 
+                              Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
+                        </div>
+                    </div>
+                </section>
+                <section>
+                    <Link to="/generic" className="image">
+                        <img src={pic10} alt="" />
+                    </Link>
+                    <div className="content">
+                        <div className="inner">
+                            <header className="major">
+                                <h3>Sed nunc ligula</h3>
+                            </header>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. 
+                              Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
+                        </div>
+                    </div>
+                </section>
             </section>
         </div>
 
